ab44f5be776c9d9cbdb3119e2b4b4efe83281bd2
Ananya Thukral <46412697+AnanyaThukral@users.noreply.github.com>
2022-01-15 01:40:51 -0500

    Login page update UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:app/screens/home_page.dart';
 import 'package:app/screens/login_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,9 +16,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       // home: HomePage(),
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
       darkTheme: ThemeData(brightness: Brightness.dark),
-      initialRoute: '/home',
+      initialRoute: '/login',
       routes: {
         '/': (context) => LoginPage(),
         '/home': (context) => HomePage(),
