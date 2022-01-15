185663e34f2e20b27db825c6f089b1cb75c5b596
Ananya Thukral <46412697+AnanyaThukral@users.noreply.github.com>
2022-01-15 00:24:49 -0500

    Add login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:app/home_page.dart';
+import 'package:app/screens/home_page.dart';
+import 'package:app/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,8 +10,19 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key); //constructor
 
-  @override //UI is built here, whenever UI refereshes build is called
+  @override //UI is built here, whenever UI refereshes build is called, context gives position of properties in the tree
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomePage());
+    return MaterialApp(
+      // home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      initialRoute: '/home',
+      routes: {
+        '/': (context) => LoginPage(),
+        '/home': (context) => HomePage(),
+        '/login': (context) => LoginPage()
+      },
+    );
   }
 }
