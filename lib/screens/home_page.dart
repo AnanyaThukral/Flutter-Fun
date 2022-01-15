d59a713187fe02c532137141aaa541851dfb9187
Ananya Thukral <46412697+AnanyaThukral@users.noreply.github.com>
2022-01-15 17:28:39 -0500

    Drawer menu added

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:app/widgets/drawer.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -6,37 +7,7 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text('intL Tracker')),
       body: Center(child: Text('Status test')),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: const <Widget>[
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-              child: Text(
-                'Drawer Header',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                ),
-              ),
-            ),
-            ListTile(
-              leading: Icon(Icons.message),
-              title: Text('Messages'),
-            ),
-            ListTile(
-              leading: Icon(Icons.account_circle),
-              title: Text('Profile'),
-            ),
-            ListTile(
-              leading: Icon(Icons.settings),
-              title: Text('Settings'),
-            ),
-          ],
-        ),
-      ),
+      drawer: MyDrawer(),
     );
   }
 }
---
+++ lib/widgets/drawer.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:cupertino_icons/cupertino_icons.dart';
+
+class MyDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final imageUrl =
+        "https://whatifgaming.com/wp-content/uploads/2021/10/32-328642_user-avatar-super-mario-avatar.png";
+    return Drawer(
+      child: Container(
+        color: Colors.deepPurple,
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              padding: EdgeInsets.zero,
+              child: UserAccountsDrawerHeader(
+                accountName: Text('Ananya Thukral'),
+                accountEmail: Text('ananya@uoguelph.ca'),
+                currentAccountPicture: CircleAvatar(
+                  radius: 30.0,
+                  backgroundImage: NetworkImage(imageUrl),
+                ),
+              ),
+            ),
+            ListTile(
+                leading: Icon(Icons.home),
+                title: Text(
+                  'Home',
+                  textScaleFactor: 1.2,
+                ),
+                iconColor: Colors.white,
+                textColor: Colors.white),
+            ListTile(
+                leading: Icon(Icons.settings),
+                title: Text(
+                  'Settings',
+                  textScaleFactor: 1.2,
+                ),
+                iconColor: Colors.white,
+                textColor: Colors.white),
+            ListTile(
+                leading: Icon(Icons.email),
+                title: Text(
+                  'Email me',
+                  textScaleFactor: 1.2,
+                ),
+                iconColor: Colors.white,
+                textColor: Colors.white)
+          ],
+        ),
+      ),
+    );
+  }
+}
