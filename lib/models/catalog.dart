5cd5ee3a615ccc88893fa7eb2a95344c4c1dab08
Ananya Thukral <46412697+AnanyaThukral@users.noreply.github.com>
2022-01-16 23:56:13 -0500

    data saved locally

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -6,5 +6,15 @@ class Item {
   final String color;
   final String image;
 
-  Item(this.id, this.name, this.desc, this.price, this.color, this.image);
+  Item({this.id, this.name, this.desc, this.price, this.color, this.image});
 }
+
+final products = [
+  Item(
+      id: "AnanyaThukral",
+      name: "iphone 12 Pro",
+      desc: "Apple iphone 12th generation",
+      price: 999,
+      color: "#33505a",
+      image: "https://pngimg.com/uploads/iphone_12/iphone_12_PNG18.png")
+];
---
+++ lib/widgets/item_widget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:app/models/catalog.dart';
+
+class ItemWidget extends StatelessWidget {
+  final Item item;
+
+  const ItemWidget({Key? key, this.item}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
